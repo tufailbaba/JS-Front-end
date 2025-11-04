078946e3652823d0660eabbec3fcdd1c9ba53725
Tufail <tufailbabacoc@gmail.com>
2025-11-04 21:23:32 +0530

    Varaibles

---
+++ Basics/index.js
@@ -0,0 +1,16 @@
+document.getElementById("a")
+// console.log(a);
+a.innerText = "This is updated text"
+// a.style.backgroundcolor = "grey"
+a.style.fontSize = "60px"
+// varaibles
+
+const b = 10;
+let c = 20;
+var d = 30;
+console.log(c);
+console.log(b);
+console.log(d);
+
+
+
