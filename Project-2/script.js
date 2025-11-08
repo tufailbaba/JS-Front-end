c6451984f5150d9c3cefd56a486a9687a29fd807
Tufail <tufailbabacoc@gmail.com>
2025-11-08 21:16:07 +0530

    Greater and lesser Game

---
+++ Project-2/script.js
@@ -0,0 +1,19 @@
+let num1= document.getElementById("num1")
+let num2= document.getElementById("num2")
+
+function solve (){
+  if(num1.value == "" || num2.value == ""){
+    num1.style.backgroundColor = "White"
+    num2.style.backgroundColor = "White"
+  }
+    else{
+        if(Number(num1.value > Number(num2.value))){
+            num1.style.backgroundColor = "green"
+            num2.style.backgroundColor = "red"
+        }
+        if(Number(num2.value > Number(num1.value))){
+             num2.style.backgroundColor = "green"
+            num1.style.backgroundColor = "red"
+        }
+    }
+}
